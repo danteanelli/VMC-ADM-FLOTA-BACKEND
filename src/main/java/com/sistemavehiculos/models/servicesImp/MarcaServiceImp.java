30e11db670449c43d4ea089958c0acc1709c52cb
gerudaeta <gerudaeta@gmail.com>
2019-06-05 15:42:46 -0300

    add metodos de marca

---
+++ src/main/java/com/sistemavehiculos/models/servicesImp/MarcaServiceImp.java
@@ -0,0 +1,39 @@
+package com.sistemavehiculos.models.servicesImp;
+
+import com.sistemavehiculos.models.dao.IMarcaDAO;
+import com.sistemavehiculos.models.entity.Marca;
+import com.sistemavehiculos.models.services.IMarcaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MarcaServiceImp implements IMarcaService {
+
+    @Autowired
+    private IMarcaDAO marcaDAO;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Marca> findAll() {
+        return (List<Marca>) marcaDAO.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Marca findById(Long id) {
+        return marcaDAO.findById(id).orElse(null);
+    }
+
+    @Override
+    public Marca save(Marca marca) {
+        return marcaDAO.save(marca);
+    }
+
+    @Override
+    public void delete(Long id) {
+        marcaDAO.deleteById(id);
+    }
+}
--- src/main/java/com/sistemavehiculos/rest/MarcaController.java
+++ src/main/java/com/sistemavehiculos/rest/MarcaController.java
@@ -1,4 +1,59 @@
 package com.sistemavehiculos.rest;
 
+import com.sistemavehiculos.models.entity.Marca;
+import com.sistemavehiculos.models.services.IMarcaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ */
+@CrossOrigin(origins = { "http://localhost:4200", "*" })
+@RestController
+@RequestMapping("/api")
 public class MarcaController {
+
+    @Autowired
+    private IMarcaService marcaService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/perfiles")
+    public List<Marca> index() {
+        return marcaService.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/perfiles/{id}")
+    public Marca show(@PathVariable Long id) {
+        return marcaService.findById(id);
+    }
+
+    @PostMapping("/perfiles")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Marca create(@RequestBody Marca marca) {
+        marca.setCreatedAt(new Date());
+        marca.setUpdatedAt(new Date());
+        return marcaService.save(marca);
+    }
+
+    @PutMapping("/perfiles/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Marca update(@RequestBody Marca perfil, @PathVariable Long id) {
+        Marca marcaActual = marcaService.findById(id);
+
+        marcaActual.setNombre(perfil.getNombre());
+        marcaActual.setUpdatedAt(new Date());
+
+        return marcaService.save(marcaActual);
+    }
+
+    @DeleteMapping("/perfiles/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        marcaService.delete(id);
+    }
 }
