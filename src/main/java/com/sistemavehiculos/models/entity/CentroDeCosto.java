3430239bcb0a351bceb0abc593213d12f87c1ff0
gerudaeta <gerudaeta@gmail.com>
2019-06-21 11:33:12 -0300

    + atributos centro de costo

--- src/main/java/com/sistemavehiculos/models/entity/CentroDeCosto.java
+++ src/main/java/com/sistemavehiculos/models/entity/CentroDeCosto.java
@@ -12,4 +12,31 @@ public class CentroDeCosto extends General {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String nroCentroCosto;
+
+    private String descripcion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNroCentroCosto() {
+        return nroCentroCosto;
+    }
+
+    public void setNroCentroCosto(String nroCentroCosto) {
+        this.nroCentroCosto = nroCentroCosto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
